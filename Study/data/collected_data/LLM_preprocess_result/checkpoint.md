ae501888cdfbb47a3546839b7f3f70c8ef10de21
Caesar <Haowen.Yang@city.ac.uk>
2023-09-01 15:42:33 +0800

    delete unnecessary .py files and introduce stage 2: LLM preprocess

---
+++ Study/data/collected_data/LLM_preprocess_result/checkpoint.md
@@ -0,0 +1,2 @@
+This is a tmd checkpoint file. 
+.
--- Study/utils/02_LLM_process.py
+++ Study/utils/02_LLM_process.py
@@ -4,12 +4,10 @@ import os
 import time
 import json
 
-openai.api_key = "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"
-
+openai.api_key = "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"
 
 def categorize_issue(pr):
     print(f"###{pr['pr_number']}###")
-
 
     # Prompt construction
     # 1
@@ -33,12 +31,21 @@ def categorize_issue(pr):
     ]
 
     response = openai.ChatCompletion.create(
+        # model="gpt-3.5-turbo-16k",
         model="gpt-4",
         messages=messages,
         max_tokens=1000
     )
 
     return response.choices[0].message['content']
+
+
+def checkpoint(checkpoint_file):
+    if not os.path.exists(checkpoint_file):
+        with open(checkpoint_file, 'w') as f:
+            # 写入文件内容
+            f.write('This is a tmd checkpoint file. \n.')
+
 
 
 if __name__ == "__main__":
@@ -51,28 +58,34 @@ if __name__ == "__main__":
         read_data = json.load(f)
 
     # Define the checkpoint file path
-    checkpoint_file = "../data/downloaded_issues_need_comments_processing/checkpoint.md"
-    if not os.path.exists(checkpoint_file):
-        with open(checkpoint_file, 'w') as f:
-            # 写入文件内容
-            f.write('This is a tmd checkpoint file. \n.')
+    checkpoint_file = "../data/collected_data/LLM_preprocess_result/checkpoint.md"
+    checkpoint(checkpoint_file)
+
+    # Define the record file
+    rcdfile_path = "../data/collected_data/LLM_preprocess_result/LLM_preprocessd_01_gpt-4-32k.txt"
 
     counter = 0
     start_time = time.time()
 
     # Loop through all the issue files in the directory and categorize each one
+
     for pr in read_data:
 
-            category = categorize_issue(pr)
+        result_each = categorize_issue(pr)
 
-            with open(file_path, 'a', encoding='utf-8') as file:
-                file.write(filename + ": " + category + "\n")
-            # print(filename + ": " + category)
+        # 对每个pr处理完直接写入
+        if not os.path.exists(rcdfile_path):
+            with open(rcdfile_path, 'w') as f:
+                f.write("\n\n" + pr['repo'] + '_' + str(pr['pr_number']) + result_each + "\n")
+        else:
+            with open(rcdfile_path, 'a', encoding='utf-8') as file:
+                file.write("\n\n" + pr['repo'] + '_' +str(pr['pr_number']) + result_each + "\n")
+        # print(filename + ": " + category)
 
-            # time sleep to ensure the limit is not interrputed
-            counter += 1
-            if counter % 3 == 0:
-                elapsed_time = time.time() - start_time
-                if elapsed_time < 60:
-                    time.sleep(60 - elapsed_time)
-                start_time = time.time()
+        # time sleep to ensure the limit is not interrputed
+        counter += 1
+        if counter % 3 == 0:
+            elapsed_time = time.time() - start_time
+            if elapsed_time < 60:
+                time.sleep(60 - elapsed_time)
+            start_time = time.time()
